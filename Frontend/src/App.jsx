22fec7c9a1650e0362453ed6dffb0e3300594ab2
vishwa.vj <iamvishwajith.s@gmail.com>
2025-04-01 02:40:59 +0530

    frontent formatting

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import { useState , useEffect} from 'react'
 import "prismjs/themes/prism-tomorrow.css"
 // import "prismjs/components/prism-jsx"
+import Editor from "react-simple-code-editor"
 import prism from "prismjs"
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [code, setCode] = useState(` function sum(){
+  return a+b;
+    `)
 
   useEffect(() => {
     prism.highlightAll()
@@ -14,13 +18,20 @@ function App() {
     <div className="main">
       <div className="left">
         <div className="code">
-          <pre>
-          <code className="language-javascript">
-            {`function sum(){
-  return a+b;
-}`}
-          </code>   
-          </pre>
+        <Editor
+              value={code}
+              onValueChange={code => setCode(code)}
+              highlight={code => prism.highlight(code, prism.languages.javascript, "javascript")}
+              padding={10}
+              style={{
+                fontFamily: '"Fira code", "Fira Mono", monospace',
+                fontSize: 16,
+                border: "1px solid #ddd",
+                borderRadius: "5px",
+                height: "100%",
+                width: "100%"
+              }}
+            />
         </div>
         <div className="review-button">Review</div>
       </div>
